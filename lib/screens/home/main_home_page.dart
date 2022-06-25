15b5156aff461dfef6276033338dfb4aa5f5e8cc
SoulCoder65 <akshaysaxena860@gmail.com>
2022-06-25 22:48:11 +0530

    Main Home Page Added and Text Widgets

---
+++ lib/screens/home/main_home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+import '../../utils/colors.dart';
+import '../../utils/colors.dart';
+
+class MainHomePage extends StatefulWidget {
+  const MainHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MainHomePageState createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              padding: const EdgeInsets.only(left: 20,right: 20),
+              margin: const EdgeInsets.only(top: 45,bottom: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "India",color: AppColors.mainColor,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SmallText(text: "Ujjain",color: Colors.black54,),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Container(
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
+                    child: Icon(Icons.search,color: Colors.white,),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
